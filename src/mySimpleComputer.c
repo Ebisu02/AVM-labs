15d0ef3c0d75ad702f657fb763a7a6dea088c3dd
Ebisu02 <71516051+Ebisu02@users.noreply.github.com>
2022-02-20 21:04:22 +0700

    Add first lab

--- src/mySimpleComputer.c
+++ src/mySimpleComputer.c
@@ -13,7 +13,13 @@ using namespace std;
 
 const char Path[] = "memory.bin";
 int memory[100];
-int commands[38] = { 0xA, 0xB, 0x;
+const int commands[38] = {
+			0xA,  0xB, 0x14, 0x15, 0x1E, 0x1F, 0x20, 0x21,
+			0x28, 0x29, 0x2A, 0x2B, 0x33, 0x34, 0x35, 0x36,
+			0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E,
+			0x3F, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46,
+			0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C };
+
 unsigned int flags = 0b00000;
 // 1 - "p" - segmentation fault
 // 2 - "0" - error of deleting on null
@@ -134,7 +140,47 @@ int sc_regGet(int register, unsigned int* value)
 
 int sc_commandEncode(int command, int operand, int * value)
 {
-	
-	return 0;
+	if (operand <= 0x7F)
+	{
+		if (std::binary_search(std::begin(commands), std::end(commands), command));
+		{
+			*value = (command << 7) | operand;
+			return 0;
+		}
+		else
+		{
+			sc_regSet(INVALID_COMMAND, 1);
+			return -1;
+		}
+	}
+	else
+	{
+		return -1;
+	}
 }
 
+int sc_commandDecode(int* command, int* operand, int value)
+{
+	int temp;
+
+	if (((value >> 14) & 1) == 0)
+	{
+		temp = value >> 7;
+		if (std::binary_search(std::begin(commands), std::end(commands), temp))
+		{
+			*command = temp;
+			*operand = value & 0x7F;
+			return 0;
+		}
+		else
+		{
+			sc_regSet(INVALID_COMMAND, 1);
+			return -1;
+		}
+	}
+	else
+	{
+		sc_regSet(INVALID_COMMAND, 1);
+		return -1;
+	}
+}
