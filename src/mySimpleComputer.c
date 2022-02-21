357dee2bfd5362b74dec6353be1ed7b4f2a5898b
SergeyLapTop <helloimclinker@gmail.com>
2022-02-21 12:07:54 +0700

    Add a header file for MySimpleComputer lib

--- src/mySimpleComputer.c
+++ src/mySimpleComputer.c
@@ -1,33 +1,6 @@
-#include <iostream>
-#include <stdlib.h>
-#include <stdio.h>
-#include <conio.h>
+#include "mySimpleComputer.h"
 
-#define SEGMENTATION_FAULT 1
-#define DIVISION_BY_ZERO 2
-#define OUT_OF_BOUNDS 3
-#define CLOCK_PULSE_IGNORE 4
-#define INVALID_COMMAND 5
-
-using namespace std;
-
-const char Path[] = "memory.bin";
-int memory[100];
-const int commands[38] = {
-			0xA,  0xB, 0x14, 0x15, 0x1E, 0x1F, 0x20, 0x21,
-			0x28, 0x29, 0x2A, 0x2B, 0x33, 0x34, 0x35, 0x36,
-			0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E,
-			0x3F, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46,
-			0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C };
-
-unsigned int flags = 0b00000;
-// 1 - "p" - segmentation fault
-// 2 - "0" - error of deleting on null
-// 3 - "M" - error out of memory
-// 4 - "T" - ignoring tact impulses
-// 5 - "E" - invalid command
-
-int sc_memoryInt()
+int sc_memoryInit()
 {
 	for (int i = 0; i < 100; ++i)
 	{
---
+++ src/mySimpleComputer.h
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SEGMENTATION_FAULT 1
+#define DIVISION_BY_ZERO 2
+#define OUT_OF_BOUNDS 3
+#define CLOCK_PULSE_IGNORE 4
+#define INVALID_COMMAND 5
+
+using namespace std;
+
+const char Path[] = "memory.bin";
+int memory[100];
+const int commands[38] = {
+			0xA,  0xB, 0x14, 0x15, 0x1E, 0x1F, 0x20, 0x21,
+			0x28, 0x29, 0x2A, 0x2B, 0x33, 0x34, 0x35, 0x36,
+			0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E,
+			0x3F, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46,
+			0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C };
+
+unsigned int flags = 0b00000;
+// 1 - "p" - segmentation fault
+// 2 - "0" - error of deleting on null
+// 3 - "M" - error out of memory
+// 4 - "T" - ignoring tact impulses
+// 5 - "E" - invalid command
+
+
+
+using namespace std;
+
+int sc_memoryInit();
+int sc_memorySet(int address, int value);
+int sc_memoryGet(int address, int * value);
+int sc_memorySave(char* filename);
+int sc_memoryLoad(char* filename);
+int sc_regInit(void);
+int sc_regSet(int register, int value);
+int sc_regGet(int register, unsigned  int* value);
+int sc_commandEncode(int command, int operand, int* value);
+int sc_commandDecode(int* command, int* operand, int value);
+
+
